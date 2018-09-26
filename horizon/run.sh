b502fb98e77a44eb37acd75d5801e040d789007f
Egon Rijpkema <git@egonrijpkema.nl>
2018-09-26 13:47:49 +0200

    Filthy hack to make sure local settings are loaded.

--- horizon/run.sh
+++ horizon/run.sh
@@ -5,4 +5,8 @@ tail -f /var/log/horizon.log &
 
 tail -f /var/log/apache2/* &
 
+cat /etc/openstack-dashboard/local_settings.py >> \
+    /usr/share/openstack-dashboard/openstack_dashboard/settings.py
+
+
 apachectl -DFOREGROUND
